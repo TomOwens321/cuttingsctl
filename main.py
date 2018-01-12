2ce4f5e5e00aaec42bf0a3276d88113aee6f297f
Demo User <debian@beaglebone.localdomain>
2018-01-12 12:59:25 -0700

    Testing to see if user can now read analog inputs - Yes.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 from lib.relay import Relay
+from lib.sensor import Sensor
 import time
 
 PIN = "P9_11"
 FREQ = 0.5      # minutes
 DURATION = 4.5   # seconds
+SENSOR = "AIN1"
 
 relay = Relay(PIN)
+sensor = Sensor(SENSOR)
 
 while(1):
     relay.timed(DURATION)
+    print "Sensor value: %f" % sensor.get_value()
     time.sleep(FREQ * 60)
